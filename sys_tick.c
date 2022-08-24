eba59302bbd0fcbac4abb6dbad8012d2cb78e809
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-08-24 16:29:15 +0200

    add SysTick timer driver

---
+++ sys_tick.c
@@ -0,0 +1,31 @@
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+#include "sys_tick.h"
+#include "common_macros.h"
+
+static void (*g_CallBackPtr)(void) = 0;
+
+void SysTick_init(uint32_t value, void (* callBackFunction )()){
+	CLEAR_BIT(NVIC_ST_CTRL_R, 0); /* disable the timer */
+	NVIC_ST_RELOAD_R = value-1; /* set compare value */
+	NVIC_ST_CURRENT_R = 0; /* clear current ticks */
+	SET_BIT(NVIC_ST_CTRL_R, 2);  /* set clock source to System Clock */
+	SET_BIT(NVIC_ST_CTRL_R, 1);  /* enable sys tick interrupt */
+	SET_BIT(NVIC_EN0_R, 15); /* enable NVIC interrupts for SysTick timer */
+	g_CallBackPtr = callBackFunction; /* set the call-back function */
+}
+
+void SysTick_start(void){
+	SET_BIT(NVIC_ST_CTRL_R, 0); /* enable timer [start counting] */
+}
+
+void SysTick_stop(void){
+	CLEAR_BIT(NVIC_ST_CTRL_R, 0); /* disable sys tick timer */
+}
+
+void SysTick_Handler(void){
+	if (g_CallBackPtr != 0){
+		(*g_CallBackPtr)();
+		NVIC_ST_CURRENT_R = 0; /* clear the count flag */
+	}
+}
---
+++ sys_tick.h
@@ -0,0 +1,27 @@
+#ifndef SYS_TICK_H_
+#define SYS_TICK_H_
+
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+
+/********************************************************/
+/* Function Prototypes */
+/********************************************************/
+
+/*
+Description: Initialize the SysTick timer with given value and a pointer to call-back function for the ISR
+					- using system clock & interrupts 
+*/
+void SysTick_init(uint32_t value, void (* callBackFunction )());
+
+/*
+Description: Enable SysTick timer to start counting
+*/
+void SysTick_start(void);
+
+/*
+Description: Disable SysTick timer
+*/
+void SysTick_stop(void);
+
+#endif /* SYS_TICK_H_ */
