692bb7428291bc380c1146284b85dd6095e29a5d
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-08-24 16:30:57 +0200

    use sysTick with interrupts instead of busywait

--- main.c
+++ main.c
@@ -1,26 +1,14 @@
 #include "application.h"
+#include "sys_tick.h"
 
 int main(void){
     
     configure_vehicle_traffic_light();
     configure_pedestrian_traffic_light();
     configure_request_button(); /* for pedestrians to request green light to pass */
+    //SysTick_init((ITERATIONS_PER_MILLISECOND)*(TRAFFIC_LIGHT_TIME_mSec), update_traffic_lights);
+    SysTick_init(100, update_traffic_lights);
+    SysTick_start();
 
-    while(1){
-        
-        /* enable red light for vehicles & green light for pedestrians, for 5 seconds */
-        switch_traffic_light_state(VEHICLE_LIGHTS, RED);
-        switch_traffic_light_state(PEDESTRIAN_LIGHTS, GREEN);
-        busy_wait_ms(TRAFFIC_LIGHT_TIME_mSec);
-
-        /* enable yellow light for both vehicles & pedestrians, for 5 seconds */
-        switch_traffic_light_state(VEHICLE_LIGHTS, YELLOW);
-        switch_traffic_light_state(PEDESTRIAN_LIGHTS, YELLOW);
-        busy_wait_ms(TRAFFIC_LIGHT_TIME_mSec);
-
-        /* enable green light for vehicles & red light for pedestrians, for 5 seconds */
-        switch_traffic_light_state(VEHICLE_LIGHTS, GREEN);
-        switch_traffic_light_state(PEDESTRIAN_LIGHTS, RED);
-        busy_wait_ms(TRAFFIC_LIGHT_TIME_mSec);
-    }
+    while(1);
 }
